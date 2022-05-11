ba6651ffaee96c245bf31cd9f3a6cfbb4f9c1bb4
Josue Gauthier <josue.gauthier@gmail.com>
2022-05-11 14:01:46 +0200

    add

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,25 @@
 import json
 from django.http import JsonResponse
 
+
+from django.forms.models import model_to_dict
+
 from products.models import Product
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
+
+        #! les 2 methodes sont equivalentes
+
         data['id'] = model_data.id
         data['title'] = model_data.title
         data['content'] = model_data.content
         data['price'] = model_data.price
+
+
+        data = model_to_dict(model_data, fields=['id','title'])
+
+        print(data)
     return JsonResponse(data)
